5552030e2a52682213e11c523d7ccc944ea2a027
apeengggg <mirfanmanaf0804@gmail.com>
2021-03-08 10:55:07 +0700

    1H

---
+++ src/SatuH.java
@@ -0,0 +1,13 @@
+
+public class SatuH {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=3, n=10, i;
+		for (i=1; i<=n; i++){
+			System.out.print(x+",");
+			x -=2;
+		}
+	}
+
+}
