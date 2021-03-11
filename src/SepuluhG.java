5abf0b1fc231223d6e991fbceb6aebdc73a6db61
apeengggg <mirfanmanaf0804@gmail.com>
2021-03-12 02:39:42 +0700

    mengerjakan sepuluh G

---
+++ src/SepuluhG.java
@@ -0,0 +1,18 @@
+
+public class SepuluhG {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		int x = 1; 
+		for (int i=1; i<=n*2; i++){
+			if((x%2)!=0){
+				System.out.print(x+",");
+				x++;
+			}else{
+				x++;
+			}
+		}
+	}
+
+}
