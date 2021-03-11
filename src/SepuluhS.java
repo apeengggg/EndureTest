67db8c44c7c11e0b42e5edff7e3c7396d08f7c34
apeengggg <mirfanmanaf0804@gmail.com>
2021-03-12 04:34:03 +0700

    mengerakan no sepuluh S

---
+++ src/SepuluhS.java
@@ -0,0 +1,15 @@
+
+public class SepuluhS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		int x = 0; 
+		for (int i=1; i<=n; i++){
+				System.out.print(x+",");
+				System.out.print(x+",");
+				x=x+1;
+		}
+	}
+
+}
