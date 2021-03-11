35590daccd63377a6fe6e7c4e9c9c4e11ae355c4
apeengggg <mirfanmanaf0804@gmail.com>
2021-03-12 02:39:08 +0700

    mengerjakan sepuluh F

---
+++ src/SepuluhF.java
@@ -0,0 +1,19 @@
+
+public class SepuluhF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		int a = 1;
+		int x = 2; 
+		for (int i=1; i<=n; i++){
+				if (i==1){
+					System.out.print(x+",");
+					a = a+1;
+			}
+				x = x*a;
+				System.out.print(x+",");
+		}
+	}
+
+}
