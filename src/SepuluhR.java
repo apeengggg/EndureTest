1cd88d622256f8d12dfd3f1a9f60747f3559d98f
apeengggg <mirfanmanaf0804@gmail.com>
2021-03-12 04:26:29 +0700

    mengerakan no sepuluh R

---
+++ src/SepuluhR.java
@@ -0,0 +1,18 @@
+
+public class SepuluhR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		int a = 3;
+		float x = 108; 
+		for (int i=1; i<=n; i++){
+				if (i==1){
+					System.out.print(x+",");
+			}
+				x = x/a;
+				System.out.print(x+",");
+		}
+	}
+
+}
