b0e4e77c06da58e4ab6cbe9ee19b1e6d3e727a4c
apeengggg <mirfanmanaf0804@gmail.com>
2021-03-12 05:32:30 +0700

    mengerakan no sepuluh W

--- src/SepuluhW.java
+++ src/SepuluhW.java
@@ -1,12 +1,12 @@
 
-public class SepuluhV {
+public class SepuluhW {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = 15;
+		int n = 16;
 		int x = 1;
 		for (int i=1; i<n; i++){
-				if((x%3)==0){
+				if((x%5)==0){
 					System.out.print("*"+",");
 				}else{
 					System.out.print(x+",");
