2a6fe7192d126d422bbd80f75442fffb2174343a
apeengggg <mirfanmanaf0804@gmail.com>
2021-03-12 04:11:35 +0700

    mengerakan no sepuluh P

---
+++ src/SepuluhP.java
@@ -0,0 +1,18 @@
+
+public class SepuluhP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		int a = 4;
+		int x = 35; 
+		for (int i=1; i<=n; i++){
+				if (i==1){
+					System.out.print(x+",");
+			}
+				x = x-a;
+				System.out.print(x+",");
+		}
+	}
+
+}
