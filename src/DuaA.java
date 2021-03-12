8230f0eac5cb073c09174194b8ca7be0abb68a1d
apeengggg <mirfanmanaf0804@gmail.com>
2021-03-12 09:17:46 +0700

    memperbaiki nomor 2a

--- src/DuaA.java
+++ src/DuaA.java
@@ -3,10 +3,10 @@ public class DuaA {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 13;
+		int a = 16;
         int b = 1;
         for (int i=1; i<=4; i++){
-            for (int j=b; j <=a; j=j+3){
+            for (int j=b; j <=a; j=j+4){
                 System.out.print(j);
                 System.out.print("\t");                                
             }
